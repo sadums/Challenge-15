b3f89b6c9fcf05d2552bb4100c7ce953a8ffc410
sadums <samjadams4@gmail.com>
2023-07-14 18:15:42 -0600

    added index db logic

--- src/client/src/js/database.js
+++ src/client/src/js/database.js
@@ -12,10 +12,21 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const result = await store.add({ text: content });
+  console.log(`RESULT: ${result}`);
+}
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await openDB('jate', 1);
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const result= await store.getAll();
+  console.log(`RESULT: ${result}`);
+  return result
+}
 
 initdb();
