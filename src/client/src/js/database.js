d1be1ffd825dca53425d757286c12bd101be8948
sadums <samjadams4@gmail.com>
2023-07-16 18:03:20 -0600

    merging

--- src/client/src/js/database.js
+++ src/client/src/js/database.js
@@ -16,7 +16,7 @@ export const putDb = async (content) => {
   const db = await openDB('jate', 1);
   const tx = db.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
-  const result = await store.add({ text: content });
+  await store.add({ text: content });
 }
 
 export const getDb = async () => {
